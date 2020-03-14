0ef419a38ab8e1198dafef9d5106c8a1cbb16fd7
Nathan Wiles <ntwiles@gmail.com>
2020-03-13 23:42:00 -0400

    Update HiLoGame.cs

--- HiLoGame.cs
+++ HiLoGame.cs
@@ -12,18 +12,22 @@ public class Program
 
     static int getGuess() => Int32.TryParse(Console.ReadLine(), out int result) ? result : getGuess();
 
-    static (int, int, bool gameWon) checkGuessEqual() => (guess = getGuess(), guessesMade++, gameWon = guess == secretNum);
+    static (int guess, int, bool gameWon) checkGuessEqual() => (guess = getGuess(), guessesMade++, gameWon = guess == secretNum);
 
-    static (bool, int guessesMade) doRound() => (Console.WriteLine(
-                checkGuessEqual().gameWon ? "You won!" :
-                guessesMade >= maxNumGuesses ? "Game over." :
-                guess > secretNum ? "Too high! " + (maxNumGuesses - guessesMade) + " guesses left." :
-                "Too low! " + (maxNumGuesses - guessesMade) + " guesses left."
+    static (bool, int guessesMade) doRound() => (
+        Console.WriteLine(
+                checkGuessEqual().guess > secretNum ? 
+                    "Too high! " + (maxNumGuesses - guessesMade) + " guesses left." :
+                guess < secretNum ? 
+                    "Too low! " + (maxNumGuesses - guessesMade) + " guesses left." :
+                    "That's it!"
         ) is object,guessesMade);
 
     static bool sayHello() => Console.WriteLine("I have a secret number from 1 to "+ maxSecretNum + ". Guess:") is object;
 
-    static void CMain(string[] args) => Console.WriteLine("", sayHello(), gameLoop(), Console.ReadKey());
+    static bool sayGoodbye() => Console.WriteLine(gameWon ? "You won!" : "Game over.") is object;
+
+    static void Main(string[] args) => Console.WriteLine("", sayHello(), gameLoop(), sayGoodbye(), Console.ReadKey());
 
     static int gameLoop() => doRound().guessesMade >= maxNumGuesses || gameWon ? 0 : gameLoop();
 }
